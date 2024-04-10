ea8edd7195750f704041dcbd6f9f86784fc77123
Vitaliy-Wyzer <105667219+Vitaliy-Wyzer@users.noreply.github.com>
2024-04-10 12:05:59 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+void utworz(float*& t, const int n) {
+    t = new float[n];
+    for (int i = 0; i<n; i++) {
+        t[i] = 0;
+    }
+}
+
+void wypelnij(float*&t, const int n, const int a, const int b) {
+    for (int i = 0; i<n; i++) {
+        t[i] = a + static_cast<float>(rand()) / RAND_MAX * b-a;
+    }
+}
+
+void wypisz(float*t, const int n) {
+    for (int i = 0; i<n; i++) {
+        std::cout << t[i] << "\t";
+    }
+    std::cout << std::endl;
+}
+
+int mail(std::string str) {
+    int pos = str.find('@');
+    if (pos != std::string::npos) {
+        return pos;
+    }
+    return -1;
+}
+
+bool selektor(std::string nazwa_out, std::string nazwa_in) {
+    std::ifstream in(nazwa_in);
+    std::ofstream out(nazwa_out, std::ios::app);
+    if (in.good() && out.good()) {
+        std::string tekst = "";
+        while (!in.eof()) {
+            in >> tekst;
+            if (mail(tekst) != -1) {
+                out << tekst << std::endl;
+            }
+        } 
+
+        in.clear();
+        in.ignore();
+        in.close();
+
+        out.clear();
+        out.close();
+
+        return 1;
+    }
+
+    std::cerr << "Blad otwarcia plikow\n";
+
+    in.clear();
+    in.ignore();
+    in.close();
+
+    out.clear();
+    out.close();
+
+    return 0;
+
+
+}
+
+int main(int argc, char** argv) {
+    if (argc!=5) {
+        std::cerr << "No nieee, poprosze 4 argumenty: <min> <max> <out.txt> <in.txt>\n";
+        return -1;
+    }
+
+    float min, max;
+    min = atof(argv[1]);
+    max = atof(argv[2]);
+
+    int n;
+    std::cout << "Prosze podac rozmiar tablicy:\n";
+    std::cin >> n;
+
+    float* t = nullptr;
+    utworz(t, n);
+    wypelnij(t, n, min, max);
+    wypisz(t, n);
+
+    const std::string file_out = argv[3];
+    const std::string file_in = argv[4];
+    if (selektor(file_out, file_in)) {
+        std::cout << "Dane zostaly zapisane w pliku " << file_out << std::endl;
+    } else {
+        std::cout << "Blad przy zapisie w plik...\n";
+    }
+
+    delete[] t;
+
+    return 0;
+}
