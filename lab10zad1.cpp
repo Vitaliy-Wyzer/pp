b9d8afa58c2ae89b3f2d5dcf85aba788c7d8f273
Vitaliy-Wyzer <105667219+Vitaliy-Wyzer@users.noreply.github.com>
2024-04-24 18:56:10 +0200

    Update lab10zad1.cpp

--- lab10zad1.cpp
+++ lab10zad1.cpp
@@ -3,160 +3,152 @@
 
 using namespace std;
 
-struct sAuto{
-	string nazwa;
-	string model;
-	int rok;
-	char skrzynia;
-	float pojemnosc;
-	int moc;
-	
-	};
+struct sAuto {
+    string nazwa, model;
+    int rok;
+    char skrzynia;
+    float pojemnosc;
+    int moc;
+};
 
-sAuto inicjuj(const string nazwa, const string model, const int rok, const char skrzynia, const float pojemnosc, const int moc){
-	sAuto samo;
-	samo.nazwa = nazwa;
-	samo.model = model;
-	samo.rok = rok;
-	samo.skrzynia = skrzynia;
-	samo.pojemnosc = pojemnosc;
-	samo.moc = moc;
-	return samo;
-	}
+sAuto inicjuj(const string nazwa, const string model, const int rok, const char skrzynia, const float pojemnosc, const int moc) {
+    sAuto sam;
 
-void inicjuj1(sAuto& samo, const string nazwa, const string model, const int rok, const char skrzynia, const float pojemnosc,const int moc){
-	samo.nazwa = nazwa;
-	samo.model = model;
-	samo.rok = rok;
-	samo.skrzynia = skrzynia;
-	samo.pojemnosc = pojemnosc;
-	samo.moc = moc;
-	}
+    sam.nazwa = nazwa;
+    sam.model = model;
+    sam.rok = rok;
+    sam.skrzynia = skrzynia;
+    sam.pojemnosc = pojemnosc;
+    sam.moc = moc;
 
-void inicjuj(sAuto* samo, const string nazwa, const string model,const int rok, const char skrzynia, const float pojemnosc, const int moc){
-	samo->nazwa = nazwa;
-	samo->model = model;
-	samo->rok = rok;
-	samo->skrzynia = skrzynia;
-	samo->pojemnosc = pojemnosc;
-	samo->moc = moc;
-	}
+    return sam;
+}
 
-sAuto wczytaj(sAuto (*wfun)(const string, const string, const int, const char, const float, const int)){
-	sAuto samo;
-	
-	cout << "Podaj nazwe samochodu: ";
-	cin >> samo.nazwa;
-	cout << "Podaj model samochodu: ";
-	cin >> samo.model;
-	cout << "Podaj rok samochodu: ";
-	cin >> samo.rok;
-	cout << "Podaj rodzaj skrzyni biegow(M/A): ";
-	cin >> samo.skrzynia;
-	cout << "Podaj pojemnosc silnika samochodu: ";
-	cin >> samo.pojemnosc;
-	cout << "Podaj moc silnika samochodu: ";
-	cin >> samo.moc;
-	
-	return wfun(samo.nazwa,samo.model,samo.rok,samo.skrzynia,samo.pojemnosc,samo.moc);
-	}
+void inicjuj1(sAuto& Auto, const string nazwa, const string model, const int rok, const char skrzynia, const float pojemnosc, const int moc) {
+    Auto.nazwa = nazwa;
+    Auto.model = model;
+    Auto.rok = rok;
+    Auto.skrzynia = skrzynia;
+    Auto.pojemnosc = pojemnosc;
+    Auto.moc = moc;
+}
 
-void wczytaj1(sAuto& samo, void (*wfun)(sAuto&, const string,const string, const int, const char, const float, const int)){
-	
-	
-	cout << "Podaj nazwe samochodu: ";
-	cin >> samo.nazwa;
-	cout << "Podaj model samochodu: ";
-	cin >> samo.model;
-	cout << "Podaj rok samochodu: ";
-	cin >> samo.rok;
-	cout << "Podaj rodzaj skrzyni biegow(M/A): ";
-	cin >> samo.skrzynia;
-	cout << "Podaj pojemnosc silnika samochodu: ";
-	cin >> samo.pojemnosc;
-	cout << "Podaj moc silnika samochodu: ";
-	cin >> samo.moc;
-	
-	wfun(samo,samo.nazwa,samo.model,samo.rok,samo.skrzynia,samo.pojemnosc,samo.moc);
-	}
+void inicjuj(sAuto* Auto, const string nazwa, const string model, const int rok, const char skrzynia, const float pojemnosc, const int moc) {
+    Auto->nazwa = nazwa;
+    Auto->model = model;
+    Auto->rok = rok;
+    Auto->skrzynia = skrzynia;
+    Auto->pojemnosc = pojemnosc;
+    Auto->moc = moc;
+}
 
-void wczytaj(sAuto* samo, void (*wfun)(sAuto*, const string,const string, const int, const char, const float, const int)){
-	
-	
-	cout << "Podaj nazwe samochodu: ";
-	cin >> samo->nazwa;
-	cout << "Podaj model samochodu: ";
-	cin >> samo->model;
-	cout << "Podaj rok samochodu: ";
-	cin >> samo->rok;
-	cout << "Podaj rodzaj skrzyni biegow(M/A): ";
-	cin >> samo->skrzynia;
-	cout << "Podaj pojemnosc silnika samochodu: ";
-	cin >> samo->pojemnosc;
-	cout << "Podaj moc silnika samochodu: ";
-	cin >> samo->moc;
-	
-	wfun(samo,samo->nazwa,samo->model,samo->rok,samo->skrzynia,samo->pojemnosc,samo->moc);
-	}
+sAuto wczytaj(sAuto (*wfun)(const string, const string, const int, const char, const float, const int)) {
+    sAuto sam;
+    
+    cout << "\nPodaj nazwe: ";
+    cin >> sam.nazwa;
+    cout << "\nPodaj model: ";
+    cin >> sam.model;
+    cout << "\nPodaj rok: ";
+    cin >> sam.rok;
+    cout << "\nPodaj skrzynie (A/M): ";
+    cin >> sam.skrzynia;
+    cout << "\nPodaj pojemnosc: ";
+    cin >> sam.pojemnosc;
+    cout << "\nPodaj moc: ";
+    cin >> sam.moc;
 
-void wyswietl(const sAuto samo){
-	cout << samo.nazwa << "\t" << samo.model << "\t" << samo.rok << "\t" << samo.skrzynia << "\t" << samo.pojemnosc << "\t" << samo.moc << "\t" << endl;
-	}
-	
-void wyswietl1(const sAuto &samo){
-	cout << samo.nazwa << "\t" << samo.model << "\t" << samo.rok << "\t" << samo.skrzynia << "\t" << samo.pojemnosc << "\t" << samo.moc << "\t" << endl;
-	}
+    return wfun(sam.nazwa, sam.model, sam.rok, sam.skrzynia, sam.pojemnosc, sam.moc);
+}
 
-void wyswietl(const sAuto *samo){
-	cout << samo->nazwa << "\t" << samo->model << "\t" << samo->rok << "\t" << samo->skrzynia << "\t" << samo->pojemnosc << "\t" << samo->moc << "\t" << endl;
-	}	
-	
-void selector(const sAuto samo){
-	if(2024 - samo.rok <= 5){
-		cout  << samo.nazwa << " " << samo.model << " ma mniej niz 5 lat:" << endl;
-		
-		}else{
-			cout << "Samochod ma wiecej niz 5 lat." << endl;
-			}
-	}	
-	
-void selector1(const sAuto& samo){
-	if(2024 - samo.rok <= 5){
-		cout  << samo.nazwa << " " << samo.model << " ma mniej niz 5 lat:" << endl;
-		}else{
-			cout << "Samochod ma wiecej niz 5 lat." << endl;
-			}
-	}
-	
-void selector(const sAuto* samo){
-	if(2024 - samo->rok <= 5){
-		cout  << samo->nazwa << " " << samo->model << " ma mniej niz 5 lat:" << endl;
-		}else{
-			cout << "Samochod ma wiecej niz 5 lat." << endl;
-			}
-	}			
+void wczytaj1(sAuto& Auto, void (*wfun)(sAuto&, const string, const string, const int, const char, const float, const int)) {
+    cout << "\nPodaj nazwe: ";
+    cin >> Auto.nazwa;
+    cout << "\nPodaj model: ";
+    cin >> Auto.model;
+    cout << "\nPodaj rok: ";
+    cin >> Auto.rok;
+    cout << "\nPodaj skrzynie (A/M): ";
+    cin >> Auto.skrzynia;
+    cout << "\nPodaj pojemnosc: ";
+    cin >> Auto.pojemnosc;
+    cout << "\nPodaj moc: ";
+    cin >> Auto.moc;
+}
+
+void wczytaj(sAuto* Auto, void (*wfun)(sAuto*, const string, const string, const int, const char, const float, const int)) {
+    cout << "\nPodaj nazwe: ";
+    cin >> Auto->nazwa;
+    cout << "\nPodaj model: ";
+    cin >> Auto->model;
+    cout << "\nPodaj rok: ";
+    cin >> Auto->rok;
+    cout << "\nPodaj skrzynie (A/M): ";
+    cin >> Auto->skrzynia;
+    cout << "\nPodaj pojemnosc: ";
+    cin >> Auto->pojemnosc;
+    cout << "\nPodaj moc: ";
+    cin >> Auto->moc;
+}
+
+void wyswietl(sAuto Auto) {
+    cout << Auto.nazwa << "\t" << Auto.model << "\t" << Auto.rok << "\t" << Auto.skrzynia << "\t" << Auto.pojemnosc << "\t" << Auto.moc << "\n";
+}
+
+void wyswietl1(sAuto& Auto) {
+    cout << Auto.nazwa << "\t" << Auto.model << "\t" << Auto.rok << "\t" << Auto.skrzynia << "\t" << Auto.pojemnosc << "\t" << Auto.moc << "\n";
+}
+
+void wyswietl(sAuto* Auto) {
+    cout << Auto->nazwa << "\t" << Auto->model << "\t" << Auto->rok << "\t" << Auto->skrzynia << "\t" << Auto->pojemnosc << "\t" << Auto->moc << "\n";
+}
+
+void selektor(sAuto Auto) {
+    if (Auto.rok < 2019) {
+        cout << "Samochod ma wiecej niz 5 lat!\n";
+    } else {
+        cout << "Samochod ma nie wiecej niz 5 lat!\n";
+    }
+}
+
+void selektor1(sAuto& Auto) {
+    if (Auto.rok < 2019) {
+        cout << "Samochod ma wiecej niz 5 lat!\n";
+    } else {
+        cout << "Samochod ma nie wiecej niz 5 lat!\n";
+    }
+}
+
+void selektor(sAuto* Auto) {
+    if (Auto->rok < 2019) {
+        cout << "Samochod ma wiecej niz 5 lat!\n";
+    } else {
+        cout << "Samochod ma  nie wiecej niz 5 lat!\n";
+    }
+}
 
 int main() {
-	sAuto sam1 = {"Toyota", "Corolla Cross", 2023, 'A',2.0,187};
-	wyswietl(sam1);
-	
-	sAuto sam2 = wczytaj(inicjuj);
-	cout << "***** Zwykla zmiena: *****" << endl;
-	wyswietl(sam2);
-	selector(sam2);
-	
-	cout << "******** Zmiena referencyjna ********" << endl;
-	sAuto& r_sam = sam1;
-	wczytaj1(r_sam,inicjuj1);
-	wyswietl1(r_sam);
-	selector1(r_sam);
-	
-	cout << "******** Zmiena wskaznikowa ********" << endl;
-	sAuto* w_sam = &sam2;
-	wczytaj(w_sam,inicjuj);
-	wyswietl(w_sam);
-	selector(w_sam);
-  
-  
-  return 0;
+    
+    sAuto sam1 = {"Hyundai", "Elantra", 2021, 'A', 2.6, 190};
+    wyswietl(sam1);
+
+    cout << "\n***** Zwykla zmienna *****\n";
+    sAuto sam2 = wczytaj(inicjuj);
+    wyswietl(sam2);
+    selektor(sam2);
+
+    cout << "\n***** Zmienna referencyjna *****\n";
+    sAuto& r_sam = sam1;
+    wczytaj1(r_sam, inicjuj1);
+    wyswietl1(r_sam);
+    selektor(r_sam);
+
+    cout << "\n***** Zmienna wskaznikowa *****\n";
+    sAuto* w_sam = &sam2;
+    wczytaj(w_sam, inicjuj);
+    wyswietl(w_sam);
+    selektor(w_sam);
+
+    return 0;
 }
+
