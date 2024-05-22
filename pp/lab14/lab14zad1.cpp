c36316306729f2c89ee31e49fa31cbf9b4c83b0c
Vitaliy-Wyzer <105667219+Vitaliy-Wyzer@users.noreply.github.com>
2024-05-22 19:35:17 +0200

    Update lab14zad1.cpp

--- pp/lab14/lab14zad1.cpp
+++ pp/lab14/lab14zad1.cpp
@@ -1,222 +1,255 @@
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
+struct SOsoba {
+	unsigned int id;
+	string email;
+	unsigned rok;
+	string imie;
+	
+	SOsoba() : id(0), email(""), rok(0), imie("") {}
+	
+	SOsoba wczytaj(ifstream&);
+	bool wypisz(SOsoba&, ostream&);
+};
+
 struct SLista {
-    struct SOsoba {
-        unsigned int id;
-        string email;
-        unsigned int rok;
-        string imie;
-    };
+	string nazwa;
+	unsigned int liczba;
+	SOsoba* osoby;
+	
+	SLista() : nazwa(""), liczba(0), osoby(nullptr) {}
+	
+	bool wczytaj(SLista&, ifstream&);
+	bool wypisz(SLista&, ostream&);
+	SOsoba szukaj(SLista&, const unsigned int);
+	void podzial(SLista&, ofstream&, ofstream&);
+};
 
-    string nazwa_listy;
-    SOsoba* osoby;
-    int liczba_osob;
+SOsoba SOsoba::wczytaj(ifstream& fin) {
+	SOsoba osoba;
+	if (!fin.good()) {
+		cerr << "Blad otwarcia pliku wejsciowego\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		exit(0);
+	}
+	
+	fin >> osoba.id;
+	if(!fin.good()) {
+		cerr << "Blad wczytywania id\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		exit(0);
+	}
+	fin >> osoba.email;
+	if(!fin.good()) {
+		cerr << "Blad wczytywania emailu\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		exit(0);
+	}
+	fin >> osoba.rok;
+	if(!fin.good()) {
+		cerr << "Blad wczytywania roku urodzenia\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		exit(0);
+	}
+	fin >> osoba.imie;
+	if(!fin.good()) {
+		cerr << "Blad wczytywania imienia\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		exit(0);
+	}
+	return osoba;
+}
 
-    SOsoba wczytaj(ifstream& in) {
-        if(!in.good()) {
-            cerr << "Blad strumienia wejsciowego.\n";
-            in.ignore();
-            in.clear();
-            in.close();
-            exit(0);
-        }
-        SOsoba os;
-        in >> os.id;
-        if(!in.good()) {
-            cerr << "Blad wczytywania id.\n";
-            in.ignore();
-            in.clear();
-            in.close();
-            exit(0);
-        }
-        in >> os.email;
-        if(!in.good()) {
-            cerr << "Blad wczytywania e-maila.\n";
-            in.ignore();
-            in.clear();
-            in.close();
-            exit(0);
-        }
-        in >> os.rok;
-        if(!in.good()) {
-            cerr << "Blad wczytywania roku urodzenia.\n";
-            in.ignore();
-            in.clear();
-            in.close();
-            exit(0);
-        }
-        in >> os.imie;
-        if(!in.good()) {
-            cerr << "Blad wczytywania imienia.\n";
-            in.ignore();
-            in.clear();
-            in.close();
-            exit(0);
-        }
-        return os;
-    }
+bool SOsoba::wypisz(SOsoba& osoba, ostream& out = cout) {
+	if (!out.good()) {
+		cerr << "Blad otwarcia pliku wyjsciowego\n";
+		out.clear();
+		return 0;
+	}
+	if (!(out << osoba.id << " ")) {
+		cerr << "Blad wypisania id\n";
+		out.clear();
+		return 0;
+	}
+	if (!(out << osoba.email << " ")) {
+		cerr << "Blad wypisania email\n";
+		out.clear();
+		return 0;
+	}
+	if (!(out << osoba.rok << " ")) {
+		cerr << "Blad wypisania roku urodzenia\n";
+		out.clear();
+		return 0;
+	}
+	if (!(out << osoba.imie << endl)) {
+		cerr << "Blad wypisania imienia\n";
+		out.clear();
+		return 0;
+	}
+	return 1;
+}
 
-    bool wypisz(SOsoba& os, ostream& out = cout) {
-        if(!out.good()) {
-            cerr << "Blad otwarcia strumienia.\n";
-            out.clear();
-            return false;
-        }
-        out << os.id;
-        if(!out.good()) {
-            cerr << "Blad wypisywania id.\n";
-            out.clear();
-            return false;
-        }
-        out << " " << os.email;
-        if(!out.good()) {
-            cerr << "Blad wypisywania e-maila.\n";
-            out.clear();
-            return false;
-        }
-        out << " " << os.rok;
-        if(!out.good()) {
-            cerr << "Blad wypisywania roku urodzenia.\n";
-            out.clear();
-            return false;
-        }
-        out << " " << os.imie << endl;
-        if(!out.good()) {
-            cerr << "Blad wypisywania imienia.\n";
-            out.clear();
-            return false;
-        }
-        return true;
-    }
+bool SLista::wczytaj(SLista& lista, ifstream& fin) {
+	if (!fin.good()) {
+		cerr << "Blad otwarcia pliku wejsciowego\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		return 0;
+	}
+	fin >> lista.nazwa;
+	if (!fin.good()) {
+		cerr << "Blad wczytywania nazwy\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		return 0;
+	}
+	fin >> lista.liczba;
+	if (!fin.good()) {
+		cerr << "Blad wczytywania liczby osob\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		return 0;
+	}
+	if (0 >= lista.liczba) {
+		cerr << "Libcza osob musi byc >= 1\n";
+		fin.ignore();
+		fin.clear();
+		fin.close();
+		return 0;
+	}
+	lista.osoby = new SOsoba[lista.liczba];
+	for (unsigned int i=0; i<lista.liczba; i++) {
+		lista.osoby[i] = lista.osoby[i].wczytaj(fin);
+		if (lista.osoby[i].id == 0 || lista.osoby[i].email == "" ||
+			lista.osoby[i].rok == 0 || lista.osoby[i].imie == "") {
+			cerr << "Blad wczytywania danych z pliku\n";
+			fin.ignore();
+			fin.clear();
+			fin.close();
+			return 0;	
+		}
+	}
 
-    bool wczytaj(SLista& l, ifstream& in) {
-        if(!in.good()) {
-            cerr << "Blad otwarcia pliku wejsciowego.\n";
-            in.ignore();
-            in.clear();
-            in.close();
-            return false;
-        }
-        in >> l.nazwa_listy;
-        if(!in.good()) {
-            cerr << "Blad wczytywania nazwy listy.\n";
-            in.ignore();
-            in.clear();
-            in.close();
-            return false;
-        }
-        in >> l.liczba_osob;
-        if(!in.good()) {
-            cerr << "Blad wczytywania liczby osob.\n";
-            in.ignore();
-            in.clear();
-            in.close();
-            return false;
-        }
-		if(l.liczba_osob<=0) {
-            cerr << "Liczba osob powinna być wieksza od zera.\n";
-            return false;
-        }
-        l.osoby = new SOsoba[l.liczba_osob];
-        for(int i=0; i<l.liczba_osob; ++i) {
-            l.osoby[i] = wczytaj(in);
-        }
-        return true;
-    }
+	return 1;
+}
 
-    bool wypisz(SLista& l, ostream& out = cout) {
-        if(!out.good()) {
+bool SLista::wypisz(SLista& lista, ostream& out = cout) {
+      if(!out.good()) {
             cerr << "Blad strumienia.\n";
             out.clear();
             return false;
         }
-        out << l.nazwa_listy << endl;
+        out << lista.nazwa << endl;
         if(!out.good()) {
             cerr << "Blad wypisywania nazwy listy.\n";
             out.clear();
             return false;
         }
-        out << l.liczba_osob << endl;
+        out << lista.liczba << endl;
         if(!out.good()) {
             cerr << "Blad wypisywania liczby osob.\n";
             out.clear();
             return false;
         }
-        for(int i=0; i<l.liczba_osob; ++i) {
-            if(!wypisz(l.osoby[i], out)) {
+        for(unsigned int i=0; i<lista.liczba; ++i) {
+            if(!lista.osoby[i].wypisz(lista.osoby[i], out)) {
                 cerr << "Blad wypisywania " << i + 1 << ". osoby.\n";
                 return false;
             }
         }
         return true;
-    }
+}
 
-    SOsoba szukaj(SLista& l, unsigned int id) {
-        for(int i=0; i<l.liczba_osob; ++i) {
-            if(l.osoby[i].id == id) {
-                return l.osoby[i];
+SOsoba SLista::szukaj(SLista& lista, const unsigned int id) {
+	for(unsigned int i=0; i<lista.liczba; ++i) {
+		if(lista.osoby[i].id == id) {
+			return lista.osoby[i];
+        }
+    }
+	exit(0);
+}
+
+void SLista::podzial(SLista& lista, ofstream& fout1, ofstream& fout2) {
+	for(unsigned int i=0; i<lista.liczba; ++i) {
+		if(lista.osoby[i].imie.back() == 'a') {
+			if(!lista.osoby[i].wypisz(lista.osoby[i], fout1)) {
+				cerr << "Blad\n";
+                exit(0);
+            }
+        } else {
+            if(!lista.osoby[i].wypisz(lista.osoby[i], fout2)) {
+                cerr << "Blad\n";
+                exit(0);
             }
         }
-        return {0, "", 0, ""}; 
     }
+}
 
-    void podzial(SLista& l, ofstream& plik_k, ofstream& plik_m) {
-        for(int i=0; i<l.liczba_osob; ++i) {
-            if(l.osoby[i].imie.back() == 'a' || l.osoby[i].imie.back() == 'A') {
-                if(!wypisz(l.osoby[i], plik_k)) {
-                    cerr << "Napotkano blad.\n";
-                    exit(0);
-                }
-            } else {
-                if(!wypisz(l.osoby[i], plik_m)) {
-                    cerr << "Napotkano blad.\n";
-                    exit(0);
-                }
-            }
-        }
-    }
-
-    void usun() {
-        if(osoby) {
-            delete[] osoby;
-            osoby = nullptr;
-        }
-    }
-};
+void usun(SLista& lista) {
+	if (lista.osoby != nullptr) {
+		delete [] lista.osoby;
+		lista.osoby = nullptr;
+	}
+}
 
 int main(int argc, char** argv) {
-    if(argc!=5) {
-        cerr << "Podano nieprawidlowa liczbe argumentow. Prawidlowe uzycie: ./nazwa <nazwaPlikuWejsciowego> <nazwaPlikuWyjsciowegoKobiety> <nazwaPlikuWyjsciowegoMezczyzni> <id>.\n";
-        return 1;
-    }
-
-    ifstream infile(argv[1]);
-    ofstream outfile_k(argv[2]);
-    ofstream outfile_m(argv[3]);
+	// plik wej, plik wyj, plik wyj m, plik wyj f, id
+	if (argc != 5) {
+		cerr << "Prosze podac argumenty: <plik wej> <plik wyj k> <plik wyj m>\n";
+		return -1;
+	}
+	
+	ifstream fin(argv[1]);
+    ofstream fout_k(argv[2]);
+    ofstream fout_m(argv[3]);
+    
+    if (!(fin.good() || fout_k.good() || fout_m.good())) {
+		cerr << "Blad otwarcia plikow\n";
+		fin.ignore();
+		fin.clear();
+		fout_k.clear();
+		fout_m.clear();
+		fin.close();
+		fout_k.close();
+		fout_m.close();
+		return -1;
+	}
 
     SLista lista;
-    if(lista.wczytaj(lista, infile)) {
+    if(lista.wczytaj(lista, fin)) {
         lista.wypisz(lista);
-        SLista::SOsoba osoba = lista.szukaj(lista, atoi(argv[4]));
+        SOsoba osoba = lista.szukaj(lista, atoi(argv[4]));
         if(osoba.id!=0) { 
-            lista.wypisz(osoba);
-            lista.podzial(lista, outfile_k, outfile_m);
+            lista.osoby[0].wypisz(osoba);
+            lista.podzial(lista, fout_k, fout_m);
         } else {
             cerr << "Nie znaleziono osoby o podanym id.\n";
             return 1;
         }
     }
 
-    lista.usun();
+    usun(lista);
 
-    infile.close();
-    outfile_k.close();
-    outfile_m.close();
+    fin.close();
+    fout_k.close();
+    fout_m.close();
 
     return 0;
 }
